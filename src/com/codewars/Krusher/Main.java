6dd1d3af8da0dccd5605ca5ed995bcd9d1a85f01
Krusher-git <64028529.Krusher-git@users.noreply.github.com>
2020-11-18 16:27:11 +0300

    Second commit First function works well Second does not work

--- src/com/codewars/Krusher/Main.java
+++ src/com/codewars/Krusher/Main.java
@@ -3,58 +3,66 @@ package com.codewars.Krusher;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//
+//        System.out.println(encode("hey"));
+//
+//    }
 
-
-        System.out.println(encode("hey"));
-
-    }
-
-    public static String encode(String text) {
+    public String encode(String text) {
 
         String[] binaryValue = new String[text.length()];
+
         for (int i = 0; i < text.length(); i++) {
             int tmp = text.charAt(i);
-            if (Integer.toBinaryString(tmp).length() != 8) {
-                binaryValue[i] = ('0' + Integer.toBinaryString(tmp));
-                System.out.println(binaryValue[i]);
+            if (Integer.toBinaryString(tmp).length() < 8) {
+                StringBuilder addSomeValues = new StringBuilder();
+                for (int j = 0; j < (8 - Integer.toBinaryString(tmp).length()); j++) {
+                    addSomeValues.append('0');
+                }
+                binaryValue[i] = addSomeValues.toString() + Integer.toBinaryString(tmp);
             } else binaryValue[i] = Integer.toBinaryString(tmp);
-
         }
         StringBuilder tripledValue = new StringBuilder();
         for (int i = 0; i < binaryValue.length; i++) {
             for (int j = 0; j < binaryValue[i].length(); j++) {
-                for (int k = 0; k < 3; k++) {
-                    tripledValue.append(binaryValue[i].charAt(j));
-                }
+                tripledValue.append(binaryValue[i].charAt(j))
+                        .append(binaryValue[i].charAt(j))
+                        .append(binaryValue[i].charAt(j));
             }
         }
         return tripledValue.toString();
     }
 
-    public static String decode(String bits) {
+    public String decode(String bits) {
         String[] converter = new String[bits.length() / 3];
-        for (int i = 0; i < converter.length; i += 3) {
-            converter[i] = bits.substring(i, i + 2);
+        for (int i = 0; i < bits.length(); i += 3) {
+            converter[i/3] = bits.substring(i, i + 3);
+            System.out.println(converter[i/3]);
             int check = 0;
-            for (int j = 0; j < converter[i].length(); j++) {
-                if (converter[i].charAt(j) == '1') {
+            for (int j = 0; j < converter[i/3].length(); j++) {
+                if (converter[i/3].charAt(j) == '1') {
                     check++;
                 } else check--;
             }
             if (check == 0) {
-                throw new IllegalArgumentException("Something goes wrong!");
+                System.out.println("дичь");
             } else if (check > 0) {
-                converter[i] = "1";
-            } else converter[i] = "0";
+                converter[i/3] = "1";
+            } else converter[i/3] = "0";
         }
-        String[] number = new String[converter.length / 8];
-        char[] almostDone = new char[number.length];
-        for (int i = 0; i < number.length; i++) {
+//        String[] number = new String[converter.length / 8];
+
+        char[] almostDone = new char[converter.length / 8];
+
+        for (int i = 0; i < almostDone.length; i++) {
+            StringBuilder checker = new StringBuilder();
             for (int j = 0; j < 8; j++) {
-                number[i] += converter[j];
+//                number[i] += converter[j];
+                checker.append(converter[j]);
             }
-            almostDone[i] = (char) Integer.parseInt(number[i], 2);
+            almostDone[i] = (char) Integer.parseInt(checker.toString(), 2);
         }
         return String.valueOf(almostDone);
     }
